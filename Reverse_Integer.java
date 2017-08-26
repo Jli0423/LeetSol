cfa8c49bd0eb332a6a9b4bed80fc0bef4130c047
jli0423 <justin.li0423@gmail.com>
2017-08-26 16:57:08 -0400

    Number 7 complete

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,3 +1,19 @@
+// Current solution
+class Solution {
+    public int reverse(int x) {
+        long ans = 0;
+        int track = 1;
+        while(x != 0){
+            ans = ans* 10 + x%10;
+            x = x/10;
+            if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE){
+                return 0;
+            }
+        }
+        return (int)ans;
+    }
+}
+
 // Slow solution
 class Solution {
     public int reverse(int x) {
@@ -16,5 +32,32 @@ class Solution {
             return 0;
         }
         return (int) value;
+    }
+}
+
+// EVEN SLOWER
+class Solution {
+    public int reverse(int x) {
+        boolean check = false;
+        if(x < 0){
+            check = true;
+            x = x*(-1);
+        }
+        double ans = 0;
+        int length = String.valueOf(x).length() - 1;
+        int digit = 1;
+        for(int i = 0; i < 10; i++){
+            ans = ans + (x%(10*digit))*Math.pow(10, length);
+            x = (x - x%(10*digit))/10;
+            length --;
+            System.out.println(ans);
+        }
+        if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE){
+            return 0;
+        }
+        if(check){
+            ans = ans*(-1);
+        }
+        return (int)ans;
     }
 }
