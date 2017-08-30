c4cef180f04717fe039f8f939b5b481f45396093
jli0423 <justin.li0423@gmail.com>
2017-08-30 12:49:08 -0400

    Number 9: solution 2

--- Palindrome_Number.java
+++ Palindrome_Number.java
@@ -11,3 +11,35 @@ class Solution {
         return false;
     }
 }
+
+
+// Solution 2: pointing to middle of array
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+        if(s.length()%2 == 1){
+            int i = s.length()/2 + 1;
+            int j = i - 2;
+            while(i < s.length() || j >= 0){
+                if(s.charAt(i) == s.charAt(j)){
+                    i++;
+                    j--;
+                }else{
+                    return false;
+                }
+            }
+        }else{
+            int i = s.length()/2;
+            int j = i - 1;
+            while(i < s.length() || j >= 0){
+                if(s.charAt(i) == s.charAt(j)){
+                    i++;
+                    j--;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
