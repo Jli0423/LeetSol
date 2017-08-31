83374db31f8a48ab78d5bed9a406c8622517b8bb
jli0423 <justin.li0423@gmail.com>
2017-08-31 12:21:24 -0400

    Number 521 finished

---
+++ Longest_Uncommon_Subsequence_I.java
@@ -0,0 +1,23 @@
+// Subsequence does not even matter here, baited
+class Solution {
+    public int findLUSlength(String a, String b) {
+        int index = 0;
+        if(a.equals(b)){
+            return -1;
+        }
+        if(a.length() == 0 || a.length() < b.length()){
+            return b.length();
+        }
+        else if(b.length() == 0 || b.length() < a.length()){
+            return a.length();
+        }
+        else{
+            for(int i = 0; i < a.length(); i++){
+                if(a.charAt(i) != b.charAt(i)){
+                    index = i;
+                }
+            }
+        }
+        return index + 1;
+    }
+}
