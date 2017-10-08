dfe4d7f900a0570bff11feb61bdc402a70114224
jli0423 <justin.li0423@gmail.com>
2017-10-08 16:25:01 -0400

    Number 242 Finished

---
+++ Valid_Anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] list = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            list[(int)s.charAt(i) - 97]++;
+        }
+        for(int i = 0; i < t.length(); i++){
+            list[(int)s.charAt(i) - 97]--;
+        }
+
+        for(int i = 0; i < list.length; i++){
+            if(list[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
