d098899ca0ad269440ae3ee8acfd0833bec97da7
jli0423 <justin.li0423@gmail.com>
2019-01-09 23:37:04 -0500

    Number 807 Finished (small bug..)

---
+++ Max_Increase_to_Keep_City_Skyline.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxIncreaseKeepingSkyline = function(grid) {
+  let originalGrid = grid;
+  let verticalMax = [];
+  let horizontalMax = grid[0];
+  let maxIncrease = 0;
+  for (let i = 0; i < grid.length; i++) {
+      let tempMax = grid[i][0];
+      for (let j = 0; j < grid[i].length; j++) {
+          tempMax = grid[i][j] > tempMax ? grid[i][j] : tempMax;
+          horizontalMax[j] = horizontalMax[j] < grid[i][j] ? grid[i][j] : horizontalMax[j];
+          console.log(`i: ${i}, j: ${j}, ${grid[0][0]}`);
+      }
+      verticalMax.push(tempMax);
+  }
+  for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+          let currMin = Math.min(verticalMax[i], horizontalMax[j]);
+          // console.log(`${currMin}: ${grid[i][j]}`);
+          maxIncrease += currMin > grid[i][j] ? currMin - grid[i][j] : 0;
+      }
+  }
+  
+  return maxIncrease;
+};
